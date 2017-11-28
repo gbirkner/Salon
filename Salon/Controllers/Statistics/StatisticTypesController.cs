d8926676e0dfa69ce7f1e3395ebdb990b36ebfab
Daniel <dostermann@tsn.at>
2017-11-28 10:37:36 +0100

    added statistics menu

---
+++ Salon/Controllers/Statistics/StatisticTypesController.cs
@@ -0,0 +1,27 @@
+﻿using Salon.Models.Statistics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Salon.Controllers.Statistics
+{
+    public class StatisticTypesController : Controller
+    {
+        // GET: StatisticTypes
+        public ActionResult Index()
+        {
+            List<StatisticTypes> definedStatistics = new List<StatisticTypes>();
+            definedStatistics.Add(new StatisticTypes(
+                "Customers per TIME", "Shows the customers for a specific time span", "Report", "/"
+                ));
+
+            definedStatistics.Add(new StatisticTypes(
+                "Visits per TIME", "Shows the visits for a specific time span", "Report", "/"
+                ));
+
+            return View(definedStatistics);
+        }
+    }
+}
---
+++ Salon/Models/Statistics/StatisticTypes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Salon.Models.Statistics
+{
+    public sealed class StatisticTypes
+    {
+        [Display(Name = "Statistic name")]
+        public string StatisticName { get; set; }
+        [Display(Name = "Statistic description")]
+        public string StatisticDescription { get; set; }
+        [Display(Name = "Statistic type")]
+        public string StatisticType { get; set; }
+        [Display(Name = "Link to statistic")]
+        public string StatisticUrl { get; set; }
+
+        public StatisticTypes(string statisticName, string  statisticDescription, string statisticType, string statisticUrl)
+        {
+            StatisticName = statisticName;
+            StatisticDescription = statisticDescription;
+            StatisticType = statisticType;
+            StatisticUrl = statisticUrl;
+        }
+    }
+}
