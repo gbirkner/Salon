f69e1218f1b91cffa0ed0b47e2b2d47209f5c7e8
Daniel <dostermann@tsn.at>
2017-11-28 12:02:59 +0100

    added line chart

---
+++ Salon/Controllers/Statistics/LineChartController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Salon.Models.Statistics;
+
+namespace Salon.Controllers.Statistics
+{
+    public class LineChartController : Controller
+    {
+        // GET: LineChart
+        public ActionResult Index()
+        {
+            var chart = new LineChart();
+            return View(chart);
+        }
+    }
+}
---
+++ Salon/Models/Statistics/LineChart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Salon.Models.Statistics
+{
+    public class LineChart
+    {
+        public List<string> Label { get; set; }
+        public List<int> GraphData { get; set; }
+    }
+}
