caf7df21102d9468bbe2e928374d07f073d0588a
LocalUser <LocalUser@VM-INFORMATIK>
2017-11-28 09:41:15 +0100

    Cities, ConnectionTypes, Countries, MetaData

---
+++ Salon/Controllers/CitiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Salon.Models;
+
+namespace Salon.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private SalonEntities db = new SalonEntities();
+
+        // GET: Cities
+        public ActionResult Index()
+        {
+            var cities = db.Cities.Include(c => c.Countries);
+            return View(cities.ToList());
+        }
+
+        // GET: Cities/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cities cities = db.Cities.Find(id);
+            if (cities == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cities);
+        }
+
+        // GET: Cities/Create
+        public ActionResult Create()
+        {
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "Title");
+            return View();
+        }
+
+        // POST: Cities/Create
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CountryId,PostalCode,Title")] Cities cities)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cities.Add(cities);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "Title", cities.CountryId);
+            return View(cities);
+        }
+
+        // GET: Cities/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cities cities = db.Cities.Find(id);
+            if (cities == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "Title", cities.CountryId);
+            return View(cities);
+        }
+
+        // POST: Cities/Edit/5
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CountryId,PostalCode,Title")] Cities cities)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cities).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "Title", cities.CountryId);
+            return View(cities);
+        }
+
+        // GET: Cities/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cities cities = db.Cities.Find(id);
+            if (cities == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cities);
+        }
+
+        // POST: Cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Cities cities = db.Cities.Find(id);
+            db.Cities.Remove(cities);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Salon/Models/MetaData/Connections.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Salon.Models
+{
+    [MetadataType(typeof(Connections))]
+    partial class ConnectionsMetaData
+    {
+        [Key]
+        public int ConnectionId { get; set; }
+
+        public int ConnectionTypeId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        [Display(Name = "Connection")]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public partial class Connections
+    {
+    }
+}
