6cb9b1c5be249218fdcafcbb4ccdeb5ccdc78270
zyrotrox <philipp.isola@gmail.com>
2017-11-28 15:36:12 +0100

    Visit Views

---
+++ Salon/Models/VisitViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Salon.Models
+{
+    public class VisitShortViewModel {
+        [Key]
+        public int visitId { get; set; }
+        [Display(Name = "Datum")]
+        public DateTime created { get; set; }
+        [Display(Name = "Kunde")]
+        public Customers customer { get; set; }
+        [Display(Name = "Stylist")]
+        public AspNetUsers stylist { get; set; }
+        
+    }
+}
