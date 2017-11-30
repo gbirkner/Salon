6ed33f30e094362af97a63aed628c59e6b2bb872
Daniel <dostermann@tsn.at>
2017-11-30 08:02:16 +0100

    finished first line chart

--- Salon/Controllers/Statistics/LineChartController.cs
+++ Salon/Controllers/Statistics/LineChartController.cs
@@ -10,9 +10,19 @@ namespace Salon.Controllers.Statistics
     public class LineChartController : Controller
     {
         // GET: LineChart
-        public ActionResult Index()
+        public ActionResult LineChart()
         {
-            var chart = new LineChart();
+            var intList = new List<int>();
+            intList.Add(3);
+            intList.Add(5);
+            intList.Add(2);
+
+            var stringList = new List<string>();
+            stringList.Add("Jannuary");
+            stringList.Add("February");
+            stringList.Add("March");
+
+            var chart = new LineChart("Customers per month", "Visits", stringList, intList);
             return View(chart);
         }
     }
--- Salon/Models/Statistics/LineChart.cs
+++ Salon/Models/Statistics/LineChart.cs
@@ -1,19 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace Salon.Models.Statistics
 {
     public sealed class LineChart
     {
+        public string ChartTitle { get; set; }
+        public string DataLabel { get; set; }
         public List<string> Labels { get; set; }
-        public List<int> GraphData { get; set; }
+        public List<int> ChartData { get; set; }
 
-        public LineChart (List<string> labels = null, List<int> graphData = null)
+        public string GetDataLabelString()
         {
+            StringBuilder concatValues = new StringBuilder();
+            foreach (var item in Labels)
+            {
+                concatValues.Append($"'{item}',");
+            }
+
+            return concatValues.ToString();
+        }
+
+        public string GetChartValueString()
+        {
+            StringBuilder concatValues = new StringBuilder();
+            foreach (var item in ChartData)
+            {
+                concatValues.Append($"'{item}',");
+            }
+
+            return concatValues.ToString();
+        }
+
+        public LineChart (string chartTitle = null, string dataLabel = null, List<string> labels = null, List<int> chartData = null)
+        {
+            ChartTitle = chartTitle;
+            DataLabel = dataLabel;
             Labels = labels;
-            GraphData = graphData;
+            ChartData = chartData;
         }
     }
 }
