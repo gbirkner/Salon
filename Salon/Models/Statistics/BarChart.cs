5a0cf5beb6cf98e861ef727370a81e21b0061576
Daniel <dostermann@tsn.at>
2017-12-05 10:39:48 +0100

    dynamic line chart

---
+++ Salon/Models/Statistics/BarChart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Salon.Models.Statistics
+{
+    public class BarChart
+    {
+        public string ChartTitle { get; set; }
+        public List<string> Labels { get; set; }
+        public List<List<int>> ChartData { get; set; }
+    }
+}
--- Salon/Models/Statistics/LineChart.cs
+++ Salon/Models/Statistics/LineChart.cs
@@ -6,14 +6,38 @@ using System.Web;
 
 namespace Salon.Models.Statistics
 {
+    public partial class ChartData
+    {
+        public string DataLabel { get; set; }
+        public List<int> DataPoints { get; set; }
+        public string Color { get; set; }
+
+        public ChartData(string datalabel, List<int> datapoints, string color)
+        {
+            DataLabel = datalabel;
+            DataPoints = datapoints;
+            Color = color;
+        }
+
+        public string GetDataString()
+        {
+            StringBuilder concatString = new StringBuilder();
+            foreach (var item in DataPoints)
+            {
+                concatString.Append($"'{item}',");
+            }
+
+            return concatString.ToString();
+        }
+    }
+
     public sealed class LineChart
     {
-        public string ChartTitle { get; set; }
-        public string DataLabel { get; set; }
+        public string ChartTitle { get; set; }        
         public List<string> Labels { get; set; }
-        public List<int> ChartData { get; set; }
+        public List<ChartData> ChartData { get; set; }
 
-        public string GetDataLabelString()
+        public string GetLabelString()
         {
             StringBuilder concatValues = new StringBuilder();
             foreach (var item in Labels)
@@ -24,21 +48,9 @@ namespace Salon.Models.Statistics
             return concatValues.ToString();
         }
 
-        public string GetChartValueString()
-        {
-            StringBuilder concatValues = new StringBuilder();
-            foreach (var item in ChartData)
-            {
-                concatValues.Append($"'{item}',");
-            }
-
-            return concatValues.ToString();
-        }
-
-        public LineChart (string chartTitle = null, string dataLabel = null, List<string> labels = null, List<int> chartData = null)
+        public LineChart(string chartTitle = null, List<string> labels = null, List<ChartData> chartData = null)
         {
             ChartTitle = chartTitle;
-            DataLabel = dataLabel;
             Labels = labels;
             ChartData = chartData;
         }
