998eca5c727144e5880969d73d1374fbfeb053cd
David <davmueller@tsn.at>
2017-12-05 11:07:33 +0100

    export

--- Salon/Controllers/Reports/ReportsController.cs
+++ Salon/Controllers/Reports/ReportsController.cs
@@ -19,64 +19,57 @@ namespace Salon.Controllers.Reports
         // GET: CustomerViewModel
         public ActionResult Customers(string export = "")
         {
-            if (export == "")
-            {
-                var customer = db.Customers.Include(c => c.Cities);
-                IEnumerable<CustomersViewModel> CustomersViewModel =
-                    (from cu in customer
-                     orderby cu.LName
-                     select new CustomersViewModel
-                     {
-                         Country = cu.Cities.CountryId,
-                         CustomerId = cu.CustomerId,
-                         Description = cu.Description,
-                         Name = cu.FName + " " + cu.LName,
-                         PostalCode = cu.Cities.PostalCode,
-                         Street = cu.Street,
-                         City = cu.Cities.Title
-                     }
-                     );
+            var customer = db.Customers.Include(c => c.Cities);
+            IEnumerable<CustomersViewModel> CustomersViewModel =
+                (from cu in customer
+                 orderby cu.LName
+                 select new CustomersViewModel
+                 {
+                     Country = cu.Cities.CountryId,
+                     CustomerId = cu.CustomerId,
+                     Description = cu.Description,
+                     Name = cu.FName + " " + cu.LName,
+                     PostalCode = cu.Cities.PostalCode,
+                     Street = cu.Street,
+                     City = cu.Cities.Title
+                 }
+                 );
 
-                this.customerList = CustomersViewModel.ToList();
-                return View(this.customerList);
-            }
+            this.customerList = CustomersViewModel.ToList();
+
             if (export == "excel")
             {
-                //System.IO.File.WriteAllLines(@"C:\tmp\Salon\test.csv", this.customerList.ToArray());
+                System.IO.File.WriteAllLines(@"C:\tmp\Salon\test.csv", this.ListToStrings());
 
                 var x = 0;
-                return View();
+                return new EmptyResult();
             }
             else if (export == "pdf")
             {
                 var x = 0;
                 return View();
             }
-            return View();
+            return View(this.customerList);
         }
 
-        public ActionResult CustomersDownload()
-        {
-            var dir = new System.IO.DirectoryInfo(Server.MapPath("~/App_Data/Images/"));
-            System.IO.FileInfo[] fileNames = dir.GetFiles("*.*");
-            List<string> items = new List<string>();
 
-            foreach (var file in fileNames)
+        private List<String> ListToStrings()
+        {
+            List<String> returnValue = new List<string>();
+
+            returnValue.Add("Name;Beschreibung;Straße;PLZ;Stadt;Land");
+            foreach(var entry in this.customerList)
             {
-                items.Add(file.Name);
+                returnValue.Add(entry.Name + ";" + entry.Description + ";" + entry.Street + ";" + entry.PostalCode + ";" + entry.City + ";" + entry.Country);
             }
 
-            return View(items);
+            return returnValue;
         }
 
-        [ActionName("ExportExcel")]
-        public ActionResult ExportExcel()
-        {
-            var x = 0;
-
-            return new EmptyResult();
-        }
-
+        /// <summary>
+        /// Dispose overload
+        /// </summary>
+        /// <param name="disposing"></param>
         protected override void Dispose(bool disposing)
         {
             if (disposing)
