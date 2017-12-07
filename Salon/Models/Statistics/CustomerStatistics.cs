cfce8660a9a920024c12e921f8bb6a3c7bcb7764
LocalUser <LocalUser@VM-INFORMATIK>
2017-12-07 08:47:42 +0100

    class for customerreports

---
+++ Salon/Models/Statistics/CustomerStatistics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Salon.Models;
+
+namespace Salon.Models.Statistics
+{
+    public sealed partial class CustomerStatistics
+    {
+        public List<Models.Customers> Customers { get; set; }
+
+        public CustomerStatistics(List<Models.Customers> customers)
+        {
+            Customers = customers;
+        }
+    }
+}
