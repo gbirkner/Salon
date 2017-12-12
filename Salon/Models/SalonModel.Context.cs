b579d4986cd4c87ea21c6e86e4c8aa7bfb4643c8
mFlintstone <mFlintstone@VM-INFORMATIK>
2017-12-12 12:02:20 +0100

    Fix

--- Salon/Models/SalonModel.Context.cs
+++ Salon/Models/SalonModel.Context.cs
@@ -12,54 +12,26 @@
 
 namespace Salon.Models
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Infrastructure;
-    using System.Data.Entity.Core.Objects;
-    using System.Linq;
-    
-    public partial class SalonEntities : DbContext
+
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+
+
+public partial class SalonEntities : DbContext
+{
+    public SalonEntities()
+        : base("name=SalonEntities")
     {
-        public SalonEntities()
-            : base("name=SalonEntities")
-        {
-        }
-    
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            throw new UnintentionalCodeFirstException();
-        }
-    
-        public virtual DbSet<AspNetRoles> AspNetRoles { get; set; }
-        public virtual DbSet<AspNetUsers> AspNetUsers { get; set; }
-        public virtual DbSet<Cities> Cities { get; set; }
-        public virtual DbSet<Connections> Connections { get; set; }
-        public virtual DbSet<ConnectionTypes> ConnectionTypes { get; set; }
-        public virtual DbSet<Countries> Countries { get; set; }
-        public virtual DbSet<Customers> Customers { get; set; }
-        public virtual DbSet<Pictures> Pictures { get; set; }
-        public virtual DbSet<StepOptions> StepOptions { get; set; }
-        public virtual DbSet<Steps> Steps { get; set; }
-        public virtual DbSet<Treatments> Treatments { get; set; }
-        public virtual DbSet<TreatmentSteps> TreatmentSteps { get; set; }
-        public virtual DbSet<Visits> Visits { get; set; }
-        public virtual DbSet<VisitTasks> VisitTasks { get; set; }
-        public virtual DbSet<Settings> Settings { get; set; }
-        public virtual DbSet<Genders> Genders { get; set; }
-    
-        public virtual int AnonymizeCustomerByID(Nullable<int> customerID)
-        {
-            var customerIDParameter = customerID.HasValue ?
-                new ObjectParameter("CustomerID", customerID) :
-                new ObjectParameter("CustomerID", typeof(int));
-    
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("AnonymizeCustomerByID", customerIDParameter);
-        }
-    
-        public virtual ObjectResult<Nullable<int>> AnonymizeCustomerByDays()
-        {
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<Nullable<int>>("AnonymizeCustomerByDays");
-        }
+
+    }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        throw new UnintentionalCodeFirstException();
     }
 
 
@@ -91,9 +63,26 @@ namespace Salon.Models
 
     public virtual DbSet<VisitTasks> VisitTasks { get; set; }
 
-    public virtual DbSet<Settings> Settings { get; set; }
-
     public virtual DbSet<Genders> Genders { get; set; }
+
+
+    public virtual int AnonymizeCustomerByID(Nullable<int> customerID)
+    {
+
+        var customerIDParameter = customerID.HasValue ?
+            new ObjectParameter("CustomerID", customerID) :
+            new ObjectParameter("CustomerID", typeof(int));
+
+
+        return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("AnonymizeCustomerByID", customerIDParameter);
+    }
+
+
+    public virtual ObjectResult<Nullable<int>> AnonymizeCustomerByDays()
+    {
+
+        return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<Nullable<int>>("AnonymizeCustomerByDays");
+    }
 
 }
 
