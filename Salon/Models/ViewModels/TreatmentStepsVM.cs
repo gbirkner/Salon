25d4bf57c7ee4bb8f5f38d27144a1e4bd270a1e5
SimonF <sfreiberger@tsn.at>
2018-01-19 15:45:42 +0100

    translations

--- Salon/Models/ViewModels/TreatmentStepsVM.cs
+++ Salon/Models/ViewModels/TreatmentStepsVM.cs
@@ -11,11 +11,17 @@ namespace Salon.Models
         [Key]
         public int StepsId { get; set; }
         public int TreatmentId { get; set; }
+        [Display(Name = "Titel")]
         public string Title { get; set; }
+        [Display(Name = "Beschreibung")]
         public string Description { get; set; }
+        [Display(Name = "Sensibel")]
         public bool isSensitive { get; set; }
+        [Display(Name = "Aktiv")]
         public bool isActive{ get; set; }
+        [Display(Name = "Dauer in Min")]
         public int Duration { get; set; }
+        [Display(Name = "Reihenfolge")]
         public int Order { get; set; }
     }
 }
